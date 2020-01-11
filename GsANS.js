54df40edef7b2ab25873f6fea87cede214c7b3aa
Earthfreedom <sakane@taskbrain.co.jp>
2020-01-11 18:24:00 +0900

    GCEへの接続前準備

--- GsANS.js
+++ GsANS.js
@@ -12,11 +12,16 @@ try {
         // init
 
         const browser = await puppeteer.launch({
-            headless: false
+            headless: false,
+            args: ['--lang=ja']
         });
+
 
         const page = await browser.newPage();
         let targetElement = "";
+        await page.setExtraHTTPHeaders({
+            'Accept-Language': 'ja-JP'
+        });
 
         const client = await page.target().createCDPSession();
         await client.send('Network.emulateNetworkConditions', {
@@ -28,8 +33,8 @@ try {
 
 
         // await page.setJavaScriptEnabled(false)
-        // await page.emulate(devices['Pixel 2 XL landscape']);
-        await page.setUserAgent('Mozilla/5.0 (Linux; Android 8.0.0; Pixel 2 XL Build/OPD1.170816.004) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3679.0 Mobile Safari/537.36');
+        await page.emulate(devices['iPad Pro landscape']);
+        // await page.setUserAgent('Mozilla/5.0 (Linux; Android 8.0.0; Pixel 2 XL Build/OPD1.170816.004) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3679.0 Mobile Safari/537.36');
 
         await page.goto('https://www.facebook.com/groups/183545349248040/');
         // await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
@@ -37,7 +42,13 @@ try {
         await page.type('input[name="email"]', process.env.MAILADDRESS);
         await page.type('input[name="pass"]', process.env.PASS);
         await page.waitFor(1000);
-        await page.click('#age_ok_btn');
+        await page.click('#u_0_5');
+        await page.waitFor(20000);
+        await page.click('#u_0_au');
+        await page.waitFor(5000);
+        await page.type('input[name="message"]', "1/10 (金）【本日のオールナイト】\n利用される方は「21:00まで」にコメントください！\n①入室時間（入室最終23:30まで）\n②退室予定時刻\n③本日やることや目標");
+
+
         //age_ok_btn
 
 
@@ -51,8 +62,8 @@ try {
         // await page.waitFor(10000);
         // setTimeout(2000)
         process.on('unhandledRejection', console.dir);
-        await page.waitFor(60000);
-        await browser.close();
+        // await page.waitFor(60000);
+        // await browser.close();
     })();
 
 } catch (err) {
